da1bd902f72c0dc4bf60096f21a32dd2b3b6a4bd
paul-london <palondon@hotmail.com>
2025-12-12 09:18:21 -0500

    Render setup for FastAPI

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -6,6 +6,5 @@ def load_products(json_path="products.json"):
     Specify a .json file to read and return the corresponding pandas dataframe.
     """
     df = pd.read_json(json_path)
-    # Optional: add computed columns
-    # df['price_with_tax'] = df['price'] * 1.08
+    
     return df
